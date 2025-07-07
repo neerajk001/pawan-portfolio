0ea6bad147bbe716f303d405de98a33a36a3e366
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-07-07 13:31:41 +0530

    added number format

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -20,7 +20,7 @@ function ContactForm() {
         </div>
 
         {/* RIGHT PANEL */}
-        <form 
+        <form
           action="https://formspree.io/f/mgvybbkd"
           method="POST"
           className="lg:w-1/2 p-8 space-y-4 w-full "
@@ -49,7 +49,15 @@ function ContactForm() {
             </div>
             <div className="w-full">
               <label className="text-sm font-medium block mb-1">Your Contact*</label>
-              <input type="tel" name="phone"  placeholder="Your Contact" className="input " />
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Your Contact"
+                pattern="0?[6-9][0-9]{9}"
+                required
+                className="input"
+              />
+
             </div>
           </div>
 
